b0f0c2f219599e91bc4bef2dfcf03f709b733dfd
shuaibiqbal <shuaibiqbaldgk@gmail.com>
2025-05-14 15:55:34 +0500

    model training completed

--- training/train_model.py
+++ training/train_model.py
@@ -18,3 +18,15 @@ X = X.reshape(-1, 8, 8, 1)
 
 # Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build model
+model = Sequential([
+    Flatten(input_shape=(8, 8, 1)),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+model.save("digits_model.h5")  # Save Keras model
+joblib.dump(digits.target_names, "class_names.json")  # Save label mapping
